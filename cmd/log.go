85759b01073c01311f4a4acfccd8d7aa32e6a902
bzzz <nweather2@gmail.com>
2022-11-07 09:51:48 -0500

    Working log command!

--- cmd/log.go
+++ cmd/log.go
@@ -1,27 +1,48 @@
 /*
 Copyright © 2022 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
+	"log"
+	"strconv"
 
+	db "github.com/nicksbuggycode/531/db/sqlc"
 	"github.com/spf13/cobra"
 )
 
 // logCmd represents the log command
 var logCmd = &cobra.Command{
 	Use:   "log",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Log a lift",
+	Long:  `Accepts lift, weight, reps and will also provide a calculated max`,
+	Args:  cobra.ExactArgs(3),
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("log called")
+		wgt, err := strconv.ParseInt(args[1], 10, 32)
+		if err != nil {
+			log.Fatal(err)
+		}
+		rep, err := strconv.ParseInt(args[2], 10, 32)
+		if err != nil {
+			log.Fatal(err)
+		}
+		wgt32 := int32(wgt)
+		rep32 := int32(rep)
+		p := db.LogLiftParams{
+			Lift:   sql.NullString{String: args[0], Valid: true},
+			Weight: sql.NullInt32{Int32: wgt32, Valid: true},
+			Reps:   sql.NullInt32{Int32: rep32, Valid: true},
+		}
+		conn, err := sql.Open(dbDriver, dbSource)
+		if err != nil {
+			log.Fatal("Can't connect to DB:", err)
+		}
+		queries := db.New(conn)
+		queries.LogLift(context.Background(), p)
 	},
 }
 
